32da23b4b7227d8c5c30487a4362a41058778c97
JerameeUC <joliver30369@ucumberlands.edu>
2026-03-17 14:19:54 -0500

    3rd - Public Commit updated celery/app.py

--- forgestack/templates/celery/app.py
+++ forgestack/templates/celery/app.py
@@ -2,10 +2,11 @@ from celery import Celery
 
 REDIS_HOST = "redis"
 REDIS_PORT = {{ values.get('redis', {}).get('port', 6379) }}
+BROKER_URL = "{{ values.get('celery', {}).get('broker_url') or ('redis://redis:' ~ values.get('redis', {}).get('port', 6379) ~ '/0') }}"
 
 celery_app = Celery(
     "tasks",
-    broker=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
-    backend=f"redis://{REDIS_HOST}:{REDIS_PORT}/0",
+    broker=BROKER_URL,
+    backend=BROKER_URL,
     include=["tasks"],
 )
