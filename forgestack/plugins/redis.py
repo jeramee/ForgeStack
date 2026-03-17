5d08837506c03e9d5cde33581504554c30a8727e
JerameeUC <joliver30369@ucumberlands.edu>
2026-03-17 14:54:29 -0500

    4th - Public Commit fixed Redis and Postgres.

--- forgestack/plugins/redis.py
+++ forgestack/plugins/redis.py
@@ -10,7 +10,7 @@ class RedisPlugin(Plugin):
 
         ctx.plan.create_file(
             "docker/redis.yml",
-            template="docker/redis.yml"
+            template="redis/docker/redis.yml"
         )
 
 
